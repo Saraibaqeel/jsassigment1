97997e25748c2aa1b9fb22ca4015314e6eb26192
saraibaqeel <64917888+Saraibaqeel@users.noreply.github.com>
2022-05-19 18:23:02 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,64 @@
+//Chap 1
+//Q1
+alert("Good Morning User \nWelcome to My Site")
+
+//Q2
+alert("Error! Please enter a valid password.")
+
+
+//Q3
+alert("Welcome to JS Land... \nHappy Coding!")
+
+//Q4
+alert("Welcome to JS Land...")
+
+//Q5
+alert("Happy Coding!")
+
+//Chap 2
+//Q1
+var username="";
+
+//Q2
+var MyName="Saraib Aqeel";
+alert(MyName);
+
+//Q3
+var message="Hello World";
+alert(message);
+
+//Q4
+var Name="Jhone Doe";
+alert(Name);
+
+
+var Age=15;
+alert(Age + " Years Old");
+
+
+var Certificate="Certified Mobile Apllication Development";
+alert(Certificate);
+
+//Q5
+var Pizza="PIZZA \nPIZZ \nPIZ \nPI \nP  ";
+console.log(Pizza);
+
+//Q6
+var email="Saraibaqeel4@gmail.com";
+var myemail="My Email Address is ";
+alert (myemail + " " + email);
+ 
+//Q7
+var book="A smarter way to learn Javascript";
+var me="I am trying to learn from ";
+alert (me + " " + book);
+
+ 
+//Q8
+document.write("Yah! I can write HTML content through Javascript");
+
+//Q9
+var design="\n▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
+document.write(design);
+
